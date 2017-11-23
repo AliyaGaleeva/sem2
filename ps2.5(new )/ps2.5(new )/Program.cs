405b24ba9cbaee3e167f0d209ca552b9a7be8989
Aliya Galeeva <aliyamaratovna1@gmail.com>
2017-11-23 20:40:20 +0300

    a

---
+++ ps2.5(new )/ps2.5(new )/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ps2._5_new__
+{
+    class Program
+    {
+        public static double Summarize(double e)
+        {
+            double a=1;
+            double sum = 0;
+            double k = 2;
+            
+            while (a > e)
+            {
+                sum = sum + a;
+                a = a * 2.0 * k * k / (2 * k) / (2 * k - 1);
+                k++;
+            }
+            return sum+a;
+        }
+        static void Main(string[] args)
+        {
+            double e= 0.000001;
+            double pi = -2;
+            pi = pi + 2 * Summarize(e);
+            Console.WriteLine(pi);
+        }
+    }
+}
---
+++ ps2.5(new )/ps2.5(new )/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("ps2.5(new )")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP Inc.")]
+[assembly: AssemblyProduct("ps2.5(new )")]
+[assembly: AssemblyCopyright("Copyright © HP Inc. 2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("765b42af-272a-401b-9af2-b6113ff867b3")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номер сборки и номер редакции по умолчанию.
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
